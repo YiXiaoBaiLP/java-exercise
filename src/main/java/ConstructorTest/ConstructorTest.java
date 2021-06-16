20f15eca81214ae58b7350e96a0bf88d76a64676
YiXiaoBai <yixiaobai.lp@qq.com>
2021-06-17 00:01:33 +0800

    "Add Java 对象初始化说明"

---
+++ src/main/java/ConstructorTest/ConstructorTest.java
@@ -0,0 +1,97 @@
+package ConstructorTest;
+
+import java.util.Random;
+
+/**
+ * 	本类的特性
+ * 		1、重载构造器
+ * 		2、用this(...)调用另一个构造器
+ * 		3、无参数构造器
+ * 		4、对象初始化模块
+ * 		5、静态初始化模块
+ * 		6、实例域初始化块
+ * 
+ * @author yixiaobai
+ *
+ */
+public class ConstructorTest {
+	public static void main(String[] args) {
+		// fill the staff array with three Employee objects
+		Employee[] staff =new Employee[3];
+		
+		staff[0] = new Employee("Harry", 40000);
+		staff[1] = new Employee(600000);
+		staff[2] = new Employee();
+		
+		// print out information about all Employee object
+		for(Employee e : staff) {
+			System.out.println("name = " + e.getName() + ", id = " + e.getId() + ", salary = " + e.getSalary());
+		}
+	}
+}
+
+class Employee {
+	private static int nextId;
+	
+	private int id;
+	private String name = ""; // instance field initalization
+	private double salary;
+	
+	/*
+	 * static initialization block
+	 * 		静态初始化域
+	 */
+	static {
+		Random generator = new Random();
+		// set nextId to a random number between 0 and 9999
+		// 给 nextId设置随机数 0～9999
+		nextId = generator.nextInt();
+	}
+	
+	/*
+	 * 	object initialization block
+	 * 	初始化域
+	 * 
+	 */
+	{
+		id = nextId;
+		nextId++;
+	}
+
+	/*
+	 * 	three overloaded constructors
+	 */
+	public Employee(String n, double s) {
+		name = n;
+		salary = s;
+	}
+	
+	public Employee(double s) {
+		// calls the Employee(String, double) constructor
+		this("Employee #" + nextId, s);
+	}
+	
+	/*
+	 * 	the default constructor
+	 */
+	public Employee() {
+		/*
+		 * name initialized to "" -- see above
+		 * salary not explicitly set -- initialized to 0
+		 * id initialized in initialization block
+		 * 
+		 */
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public int getId() {
+		return id;
+	}
+}
---
+++ src/main/java/ParamTest/ParamTest.java
@@ -0,0 +1,86 @@
+package	ParamTest;
+
+public class ParamTest {
+	public static void main(String[] args) {
+		
+		/*
+		 *  Test 1: Methods can't modify numeric parameters
+		 *  	1: 一个方法不能修改一个基本数据类型的参数。
+		 */
+		System.out.println("Testing tripleValue");
+		double percent = 10;
+		System.out.println("Before: percent = " + percent);
+		tripleValue(percent);
+		System.out.println("After: percent = " + percent);
+		
+		/*
+		 *	Test 2: Methods can change the state of object parameters 
+		 * 		2: 一个方法可以改变一个对象参数的状态
+		 */
+		System.out.println("\nTesting tripleSalary:");
+		Employee harry = new Employee("Harry", 50000);
+		System.out.println("Before: salary = " + harry.getSalary());
+		tripleSalary(harry);
+		System.out.println("After: salary = " + harry.getSalary());
+		
+		/*
+		 *  Test 3: Methods can't attach new object to object parameters
+		 * 	Test 3: 一个方法不能让对象参数引用一个新的对象
+		 */
+		System.out.println("\nTesting swap:");
+		Employee a = new Employee("Alice", 7000);
+		Employee b = new Employee("Bob", 6000);
+		System.out.println("Before: a = " + a.getName());
+		System.out.println("Before: b = " + b.getName());
+		swap(a, b);
+		System.out.println("After: a = " + a.getName());
+		System.out.println("Aftre: b = " + b.getName());
+	}
+	
+	public static void tripleValue(double x) {
+		x = 3 * x;
+		System.out.println("End of method " + x);
+	}
+	
+	public static void tripleSalary(Employee x) {
+		
+		x.raiseSalary(200);
+		System.out.println("End of method salary = " + x.getSalary());
+	}
+	
+	public static void swap(Employee x, Employee y) {
+		
+		Employee temp = x;
+		x = y;
+		y = temp;
+		System.out.println("End of method : x = " + x.getName());
+		System.out.println("End of method : y = " + y.getName());
+		
+	}
+}
+
+class Employee {
+	
+	private String name;
+	private double salary;
+	
+	public Employee(String n, double s) {
+		
+		name = n;
+		salary = s;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void raiseSalary(double byPercent) {
+		double raise = salary * byPercent / 100;
+		salary += raise;
+	}
+}
+
