84dede6acd062b777daebc3b02850c6123a1e2cf
yixiaobai <yixiaobai.lp@qq.com>
2021-11-17 00:29:56 +0800

    Stream API 初体验

---
+++ src/main/java/buzz/yixiaobai/stream/filter/FilterStreamDemo.java
@@ -0,0 +1,45 @@
+package buzz.yixiaobai.stream.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+/**
+ * 初次使用Stream流
+ * @author yixiaobai
+ *
+ */
+public class FilterStreamDemo {
+
+	// 内部迭代：迭代由Stream API完成
+	@Test
+	public void filterTest1() {
+	
+		List<String> strList = new ArrayList<>();
+		/*
+		 * 筛选与切片
+		 * 	filter：接受Lambda，从流中排除某些元素
+		 *  limit：截断流，使其元素不超过给定的数量
+		 *  skip(n)：跳过元素，返回一个扔掉了前n个元素的流，若流中元素不足n个，则返回一个空流，与limit(n)互补
+		 *  distinct：筛选，通过所产生元素的hashCode()和equals()去筛选重复元素
+		 */
+		// 中间操作：不会执行任何操作
+		Stream<String> stream = strList.stream()
+				.limit(10)
+				.skip(2)
+				.distinct()
+				.filter((e) -> {
+					System.out.println("短路");
+					return false;
+				})
+				.filter((e) -> {
+					System.out.println("Stream API的中间操作");
+					return true;
+				});
+		// 终止操作：一次性执行全部内容，即“惰性求值”
+		stream.forEach(System.out::println);
+	}
+	
+}
---
+++ src/main/java/buzz/yixiaobai/stream/map/MapStreamDemo.java
@@ -0,0 +1,55 @@
+package buzz.yixiaobai.stream.map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+/**
+ * stream映射Demo
+ * @author yixiaobai
+ *
+ */
+public class MapStreamDemo {
+
+	@Test
+	public void test() {
+		/*
+		 * 映射：
+		 * 	map：接受Lambda，将元素转换成其他形式或提取信息。接受一个函数作为参数，该函数会被引用到
+		 * 每个元素上，并将其映射成一个新元素。
+		 *  flatMap：接收一个函数作为参数，将流中的每个值都转换成利另一个流，然后把
+		 * 所有流连接成一个流。
+		 */
+		List<String> list = Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee");
+		// 获取stream流
+		list.stream()
+			.map((str) -> str.toUpperCase())
+			.forEach(System.out::println);
+		System.out.println("-----------------------------------");
+		Stream<Stream<Character>> stream = list.stream()
+				.map(MapStreamDemo :: filterCharacter);
+		stream.forEach((sm) -> {
+			sm.forEach(System.out::println);
+		});
+		System.out.println("-----------------------------------");
+		
+		Stream<Character> sm = list.stream()
+		.flatMap(MapStreamDemo :: filterCharacter);
+	}
+	
+	/**
+	 * 类似与 Stream API 中的 flatMap()方法
+	 * @param str
+	 * @return
+	 */
+	public static Stream<Character> filterCharacter(String str) {
+		List<Character> list = new ArrayList<>();
+		for(Character ch : str.toCharArray()) {
+			list.add(ch);
+		}
+		return list.stream();
+	}
+}
