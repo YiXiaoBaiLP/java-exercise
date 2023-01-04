8bd59daaec97f20d40d398b8b1830c919d9a02bb
YiXiaoBai <yixiaobai.lp@qq.com>
2023-01-04 22:59:04 +0800

    初次使用Collectors.summarizingInt()方法 获取最大值、最小值、平均值、数量信息

--- src/main/java/buzz/yixiaobai/lambda/chap6/chap6_2_1/Summarizing.java
+++ src/main/java/buzz/yixiaobai/lambda/chap6/chap6_2_1/Summarizing.java
@@ -3,6 +3,7 @@ package buzz.yixiaobai.lambda.chap6.chap6_2_1;
 import org.junit.Test;
 
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -12,9 +13,11 @@ public class Summarizing {
     public void test01() {
         // 使用比较器找到热量最高的菜肴
         findMostCaloricDishUsingComparator();
+        // 获取最大值、最小值、数量、平均值
+        getSummarizing();
     }
 
-    public static Dish findMostCaloricDishUsingComparator(){
+    public static void findMostCaloricDishUsingComparator(){
         // 获取一个卡路里比较器
         Comparator<Dish> dishCaloriesComparator = Comparator.comparingInt(Dish::getCalories);
         Optional<Dish> mostCalorieDish = Dish.menu.stream().collect(Collectors.maxBy(dishCaloriesComparator));
@@ -22,6 +25,14 @@ public class Summarizing {
         Dish.menu.stream().collect(Collectors.summarizingLong(Dish::getCalories));
         Dish.menu.stream().collect(Collectors.summarizingDouble(Dish::getCalories));
 
-        return mostCalorieDish.get();
+    }
+
+    /**
+     * 获取最大值、最小值、数量、平均值
+     */
+    public static void getSummarizing(){
+        IntSummaryStatistics menuStatistics = Dish.menu.stream().collect(Collectors.summarizingInt(Dish::getCalories));
+        long count = menuStatistics.getCount();
+        System.out.println(menuStatistics);
     }
 }
